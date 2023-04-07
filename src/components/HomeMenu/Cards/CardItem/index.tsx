59db6066eb4ade231b51862c1ce7ae032e61dbc4
felipeluis223 <luis@up4tech.com.br>
2023-04-06 21:05:49 -0300

    chore: ajuste na estilização do card

--- src/components/HomeMenu/Cards/CardItem/index.tsx
+++ src/components/HomeMenu/Cards/CardItem/index.tsx
@@ -8,13 +8,13 @@ export function CardItem(props:Products){
     const formatName = props.name;
 
     return (
-        <div className="w-[250px] h-[280px] bg-[#f5f5f5] rounded-md">
-            <h3 className="w-full h-[50px] flex justify-center items-center font-bold text-center text-xl text-[#000000]">{formatName}</h3>
+        <div className="w-[250px] h-[280px] bg-[#161616] rounded-md hover:bg-[#ffffff] duration-500	text-[#ffffff] hover:text-[#000000]">
+            <h3 className="w-full h-[50px] flex justify-center items-center font-bold text-center text-xl">{formatName}</h3>
                 <div className="w-full h-[150px] flex justify-center items-center">
                     <img src={require("../../../../img/cafe.png")} className="h-[100px]" />
                 </div>
                 <div className="w-full h-[70px] pr-[10px] pl-[10px] flex flex-col justify-center items-center">
-                    <h3 className="text-xl font-bold text-[#000000]">R$ {formatValue}</h3>
+                    <h3 className="text-xl font-bold">R$ {formatValue}</h3>
                     <button className="w-full p-[5px] bg-[#9d5631] rounded-[8px] mt-[10px] text-[#ffffff] shadow-xl">Adicionar</button>
             </div>
         </div>
